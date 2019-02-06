f5d18294ba9347d41bc4238bc0e2a2cea7b75411
AlbertoPaezTudela <albertopaez92@gmail.com>
2019-02-06 09:07:11 +0100

    update Libreria

--- Libreria/src/utilidad/Input.java
+++ Libreria/src/utilidad/Input.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Input {
-	
+
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public float tryFloat() throws IOException {
@@ -135,5 +135,77 @@ public class Input {
 		} while (failsafe);
 		return numero;
 	}
-	
+
+	public int tryIntegerAcotado(int menor, int mayor) throws IOException {
+		int numero = 0;
+		boolean failsafe = true;
+		do {
+			try {
+				numero = Integer.parseInt(br.readLine());
+				failsafe = false;
+			} catch (NumberFormatException ex) {
+				System.out.println("Comando invalido");
+			}
+			if (numero < menor || numero > mayor) {
+				System.out.println("Comando invalido");
+				failsafe = true;
+			}
+		} while (failsafe);
+		return numero;
+	}
+
+	public double tryDoubleAcotado(double menor, double mayor) throws IOException {
+		double numero = 0;
+		boolean failsafe = true;
+		do {
+			try {
+				numero = Double.parseDouble(br.readLine());
+				failsafe = false;
+			} catch (NumberFormatException ex) {
+				System.out.println("Comando invalido");
+			}
+			if (numero < menor || numero > mayor) {
+				System.out.println("Comando invalido");
+				failsafe = true;
+			}
+		} while (failsafe);
+		return numero;
+	}
+
+	public float tryFloatAcotado(float menor, float mayor) throws IOException {
+		float numero = 0;
+		boolean failsafe = true;
+		do {
+			try {
+				numero = Float.parseFloat(br.readLine());
+				failsafe = false;
+			} catch (NumberFormatException ex) {
+				System.out.println("Comando invalido");
+			}
+			if (numero < menor || numero > mayor) {
+				System.out.println("Comando invalido");
+				failsafe = true;
+			}
+		} while (failsafe);
+		return numero;
+	}
+
+	public long tryLongAcotado(long menor, long mayor) throws IOException {
+		long numero = 0;
+		boolean failsafe = true;
+		do {
+			try {
+				numero = Long.parseLong(br.readLine());
+				failsafe = false;
+			} catch (NumberFormatException ex) {
+				System.out.println("Comando invalido");
+			}
+			if (numero < menor || numero > mayor) {
+				System.out.println("Comando invalido");
+				failsafe = true;
+			}
+		} while (failsafe);
+		return numero;
+	}
+
 }
