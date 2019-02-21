17b4a55ddf6b505497491783bd5102ded48369f8
AlbertoPaezTudela <albertopaez92@gmail.com>
2019-02-21 09:42:48 +0100

    update Libreria

--- Libreria/src/utilidad/Lista.java
+++ Libreria/src/utilidad/Lista.java
@@ -1,12 +1,13 @@
 package utilidad;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Lista {
 
-	// RECIBE UN ARRAY INTEGER Y DEVUELVE UN ARRAYLIST INTEGER
-	public ArrayList<Integer> arrayIntToArrayList(int[] array) {
-		ArrayList<Integer> contenedor = new ArrayList<Integer>();
+	// RECIBE UN ARRAY INTEGER Y DEVUELVE UN LIST INTEGER
+	public List<Integer> arrayIntToList(int[] array) {
+		List<Integer> contenedor = new ArrayList<Integer>();
 
 		for (int x = 0; x < array.length; x++) {
 			contenedor.add(array[x]);
@@ -14,8 +15,8 @@ public class Lista {
 		return contenedor;
 	}
 
-	// RECIBE UN ARRAYLIST INTEGER Y DEVUELVE UN ARRAY INTEGER
-	public int[] arrayListToArrayInt(ArrayList<Integer> contenedor) {
+	// RECIBE UN LIST INTEGER Y DEVUELVE UN ARRAY INTEGER
+	public int[] ListToArrayInt(List<Integer> contenedor) {
 		int[] array = new int[contenedor.size()];
 
 		for (int x = 0; x < array.length; x++) {
